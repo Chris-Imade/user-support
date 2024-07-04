81f8f488268d8e80c0e76e2c74aecdd1e27a07f0
@Chrisdevcode <jephthahimade@gmail.com>
2024-07-04 17:52:29 +0100

    all done with charity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,20 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Business Help Center | Privacy Policy"
+  title: "Business Help Center | Privacy Policy",
+  openGraph: {
+    title: "Meta | Business Help Center | Support",
+    description:
+      "Meta Support page - Dedicated to making our meta safe for everyone",
+    images: [
+      {
+        url: "https://firebasestorage.googleapis.com/v0/b/charity-7cbc9.appspot.com/o/card_img.jpg?alt=media&token=2ea1c79f-ef3d-4516-a511-4d42a0b36940",
+        width: 800,
+        height: 600,
+        alt: "Meta Support | Help Center",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -15,9 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
